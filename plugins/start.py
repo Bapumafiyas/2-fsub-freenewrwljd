ded407ad87748d9452ab4dc9b8462757cd0ca337
Bapumafiyas <154887259+Bapumafiyas@users.noreply.github.com>
2024-09-13 22:29:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,6 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 import pyrogram.utils
 pyrogram.utils.MIN_CHANNEL_ID = -your channel id -1002239557483
-
 from bot import Bot
 from config import ADMINS, OWNER_ID, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
